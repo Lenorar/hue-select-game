20e472e095d1f45790f5829e987d8c661f395b09
Lenora Rigoni <lenorarigoni@gmail.com>
2017-08-11 15:38:16 -0400

    responsive site

--- js/main.js
+++ js/main.js
@@ -10,48 +10,60 @@ $( document ).ready(function() {
 
     $('body').css('background-color', '#E2E8BA')
 
+
+    //if first load choose this
     myDiv.append( `<h3>Welcome to Match Colors. You have a limited amount of time to make the large color block with the color bars. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In facilisis consectetur leo vitae convallis. Nam sed nulla pharetra, elementum felis tempus, sollicitudin nulla. Aenean dictum risus velit, et vestibulum orci finibus quis. Curabitur ac leo vitae nulla feugiat dictum. Nullam dignissim erat libero, nec blandit diam facilisis in.</h3>` );
     $( "body" ).append( myDiv );
+
+    //if previous click was reset-button show this
+
+
 
     myDiv.animate({
         opacity: 1,
         top: 50
     }, 2000 );
 
-    myDiv.fadeIn('slow');
 
     startButton.fadeIn( 1000);
 
     $("body").append( startButton );
 
     $(startButton).click(function(){
-        console.log('please please please work')
+        myDiv.hide();
         startGame();
     });
 
 });
 
 
+//this function changes rounds when user gets correct color
+let roundNumber = 1;
+let round = $('#round');
+function addRound(){
+    if (roundNumber < 7){
+        roundNumber+=1;
+        round.text(roundNumber);
+    } if (roundNumber > 7) {
+        finishTimer();
+        finishedState();
+    }
+}
 function startGame(){
-
+    console.log('this is the beginning layout of game')
     $('body').css('background-color', '#fff')
-
-    $('.intro').hide();
-
-    $('.container').show();
     $(startButton).hide();
-
-    //start timer
+    $('.intro').hide();
+    roundNumber = 1;
+    $('.container').show();
     resetCountDown();
     countDown();
 }
 
 
+
 $('#reset-button').click(function(){
-    console.log('clicked baby!');
-    $('.container').hide();
-    myDiv.animate({top: 30}, "slow");
-    $('body').css('background-color', '#E2E8BA')
+    window.location.reload();
 });
 
 
@@ -59,16 +71,13 @@ function advanceToNextRound() {
     console.log('yoooo')
     addPoints();
     addRound();
-
     resetCountDown();
     countDown();
     changeColor();
-    // assignColor();
 }
 
 function TimeOutNextRound() {
     addRound();
-
     resetCountDown();
     countDown();
     changeColor();
@@ -76,13 +85,30 @@ function TimeOutNextRound() {
 
 
 //this is the function that assigns the colors
-let arrayColorOptions = [[],['#B3E6FF','#ADE1FA', '#ADDAF2', '#A3DAF2', '#A3DAFF', "#95DAFF", '#CBF0FF', '#CBE7FF'], ['#795786', '#79618C', '#89618C', '#92618C', '#926198', '#9E6198', '#9E7198', '#9E718E'], ['#A22D3A','#A22D35','#A22835','#9B2835','#9B282E','#9B222E','#94222E','#941C2E'], ['#E64F43','#E64F4A', '#E6544A', '#ED544A', '#F2544A', "#F24C4A", '#F24C43', '#F24C3A'], ['#FFE975','#F4E375', '#EDE375', '#EDDB75', '#EDDB82', "#EDDB8C", '#EAD883', '#EAD87A'],['#45BE6E','#40BE6E', '#35BA6E', '#35BA60', '#238135', "#29993E", '#29B23E', '#29B25E'],['#ABD7F7','#9DC5E3', '#9DD1E3', '#8AD1E3', '#9AE8FC', "#B6E8FC", '#C0DEFC', '#BCDAF7']]
+let arrayColorOptions = [[]
+['#B3E6FF','#ADE1FA','#ADDAF2','#A3DAF2','#A3DAFF','#95DAFF','#CBF0FF','#CBE7FF'],
+['#795786','#79618C','#89618C','#92618C','#926198','#9E6198','#9E7198','#9E718E'],
+['#A22D3A','#A22D35','#A22835','#9B2835','#9B282E','#9B222E','#94222E','#941C2E'],
+['#E64F43','#E64F4A','#E6544A','#ED544A','#F2544A','#F24C4A','#F24C43','#F24C3A'],
+['#FFE975','#F4E375','#EDE375','#EDDB75','#EDDB82','#EDDB8C','#EAD883','#EAD87A'],
+['#45BE6E','#40BE6E','#35BA6E','#35BA60','#238135','#29993E','#29B23E','#29B25E'],
+['#8FC5E3','#9DC5E3','#9DD1E3','#8AD1E3','#9AE8FC','#B6E8FC','#C0DEFC','#BCDAF7']]
 
 function changeColor() {
+
+    if (arrayColorOptions.length == 1){
+      resetCountDown();
+      finishedState();
+      return
+
+    }
+
 
     arrayColorOptions.shift();
     individualColorArray = arrayColorOptions[0]
     assignColor();
+
+
 
 }
 //new function that assigns each div a color in colorarray#
@@ -129,8 +155,7 @@ function updateTime(){
 
 function countDown() {
     console.log('start');
-
-    this.timerDown = setInterval(this.updateTime.bind(this), 200);
+    this.timerDown = setInterval(this.updateTime.bind(this), 100);
 };
 
 
@@ -142,32 +167,28 @@ function resetCountDown () {
 
 
 
-//the screen generates 8 bars with random colors from an array
 
-// let colors = ['#AEBCD1','#A7BCD1','red','#A7BAD1','#A7B4D1','#A7B4BC','#9DA9B1','#9DA9B6'];
 
-// function randomColorShuffle(array) {
-//   var randomArray = array.length, newArray, randomIndex;
+function updateTimeThreeTwoOne(){
+    if (timer.seconds > 0){
+      timer.seconds--;
+      timer.timerId.textContent = timer.seconds;
+    }else {
+      startGame();
+    }
+}
 
-//   // When the array has more than 0 items
-//   while (0 !== randomArray) {
+//this function counts down from 3
+function resetCountDownEasy () {
 
-//     // randomize the indexes
-//     randomIndex = Math.floor(Math.random() * randomArray);
-//     randomArray -= 1;
+    clearTimeout(this.timerDown);
+    timer.seconds = 4;
 
-//     //  move them around
-//     newArray = array[randomArray];
-//     array[randomArray] = array[randomIndex]; //this makes sure there are no duplicates and thats its the samme
-//     array[randomIndex] = newArray;
-//     // assignDiv();
-//     console.log('randomIndex')
-//   }
-//   return array;
-// }
+    updateTimeThreeTwoOne();
 
-// randomColorShuffle(colors);
-// console.log(colors);
+    this.timerDown = setInterval(this.updateTimeThreeTwoOne.bind(this), 1000);
+
+}
 
 
 
@@ -192,7 +213,6 @@ function evaluate() {
 function finishTimer () {
     console.log('end timer');
     clearInterval(this.timerDown);
-
 }
 
 //these functions counts the users score
@@ -209,23 +229,43 @@ function subtractPoints(){
     score.text(count);
 };
 
-//this function changes rounds when user gets correct color
-let roundNumber=1;
-let round = $('#round');
 
-function addRound(){
-    if (roundNumber < 7){
-        roundNumber+=1;
-        round.text(roundNumber);
-    } if (roundNumber > 7) {
-        finishTimer();
-    }
-}
+
 
 
 function finishedState(){
-    $('.row').hide();
-    $('body').css('background-color', '#A7B4BC')
+    $('body').css('background-color', '#fbfbfb')
+    $('.color-container').hide();
+    $('.reset').hide();
+    $('#timer-container').hide();
+    $('.rounds-container').hide();
+    $('#primary-color').hide();
+
+
+    $( ".score-container" ).addClass( "final-score" );
+
+    $('.finish-state').animate({
+        opacity: 100,
+}, 2000 );
+
+    randomColorFinishState();
+    randomColorChangeEveryFewSeconds();
 }
+
+function randomColorFinishState(){
+
+
+    colorArray=['#B3E6FF','#795786','#A22D3A','#E64F43','#FFE975','#45BE6E','#45BE6E'];
+    let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+    $('.finish-state').css('border-color', randomColor);
+    $('h4').css('color', randomColor);
+
+
+    };
+setInterval(function randomColorChangeEveryFewSeconds(){randomColorFinishState(0)},1000);
+
+$('.finish-state').click(function(){
+    window.location.reload();
+});
 
 
